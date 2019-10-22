b60adad7a61d8bcbeb03ea1244dc81bbfbe0a512
PC_RYZEN <PC_RYZEN@RYZEN5>
2019-10-22 15:04:07 -0500

    01

---
+++ Sistema_Inventario/src/mysql/MySQLConexion.java
@@ -0,0 +1,30 @@
+package mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class MySQLConexion {
+	public MySQLConexion(){}
+	
+	private static String URL="jdbc:mysql://localhost:3306/db_inventario?useSSL=false";
+	private static String USER="root";
+	private static String PASS="Aa123";
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConection(){
+		Connection con = null;
+		try {
+			con=DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
