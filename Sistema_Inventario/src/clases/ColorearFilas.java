e89f04301e100d6dcfdfa6c00f18c9a3c1e651bc
reyman-escolar <52724397+reyman-escolar@users.noreply.github.com>
2020-01-18 21:36:31 -0500

    Terminado, pero parece que es el problema con windows

---
+++ Sistema_Inventario/src/clases/ColorearFilas.java
@@ -0,0 +1,30 @@
+package clases;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class ColorearFilas extends DefaultTableCellRenderer{
+
+	String cod;
+	
+	public ColorearFilas(String cod) {
+		this.cod=cod;
+	}
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean Selected, boolean hasFocus, int row, int col){
+		super.getTableCellRendererComponent(table, value, Selected, hasFocus, row, col);
+		
+		if(table.getValueAt(row,0).toString().equals(cod)){
+			setBackground(Color.red);			
+		}
+		
+		
+		return this;
+	}
+	
+
+}
