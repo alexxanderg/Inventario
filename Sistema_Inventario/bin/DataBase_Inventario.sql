fd543ff23e714acf150ae5ab21e46a590a1bebdb
Ghalex94 <agamarra@bytexbyte.com.pe>
2020-02-27 14:57:18 -0500

    swings-all

--- Sistema_Inventario/bin/DataBase_Inventario.sql
+++ Sistema_Inventario/bin/DataBase_Inventario.sql
@@ -122,7 +122,8 @@ insert into tb_configuraciones values(null,'marca,color,lote,laboratorio,fvencim
 insert into tb_clientes values(	null, 'DNI', '76784966', 'Alexander Gamarra', 'La Encalada', '986865523', 'agamarra@bytexbyte.com.pe', 0);
 insert into tb_clientes values(	null, 'RUC', '20604635447', 'BYTE x BYTE E.I.R.L.', 'Calle Octavio Muñoz Najar 213 Int 207', '986865523', 'admin@bytexbyte.com.pe', 0);
 
-insert into tb_productos values(null, '123456789', 'USB 64GB', '3.0', 'Sony', 'Negro', null, null, 'Unidad', null, 'General', 'Principal', 5, 2, 40.5, 55.9, 10, 0, null, 0, 0, null, 0, 0);
+insert into tb_productos values(null, null, 'Datos de combos', null, null, null, null, null, null, null, '.General', '.Principal', 0, 0, 0, 0, 0, 0, null, 0, 0, null, 0, 0);
+insert into tb_productos values(null, '123456789', 'USB 64GB', '3.0', 'Sony', 'Negro', null, null, 'Unidad', null, '.General', '.Principal', 5, 2, 40.5, 55.9, 10, 0, null, 0, 0, null, 0, 0);
 
 use db_inventario;
 select * from tb_usuarios;
